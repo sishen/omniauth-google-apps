760152a56a1bde8fddbaace48715eae64039c91d
Mike Skalnik <mike.skalnik@gmail.com>
2012-05-10 17:35:37 -0400

    Fixes 500 error if no URI given for discovery

--- lib/openid/gapps.rb
+++ lib/openid/gapps.rb
@@ -7,21 +7,25 @@ module OpenID
   # we need to monkeypatch it to make it play nicely
   # with others.
   def self.discover(uri)
-    discovered = self.default_discover(uri)
+    raise OpenID::DiscoveryFailure.new("No URL Provided", 200) if uri.nil?
 
-    if discovered.last.empty?
+    begin
+      discovered = self.default_discover(uri)
+
+      if discovered.last.empty?
+        info = discover_google_apps(uri)
+        return info if info
+      end
+
+      return discovered
+    rescue OpenID::DiscoveryFailure => e
       info = discover_google_apps(uri)
-      return info if info
-    end
 
-    return discovered
-  rescue OpenID::DiscoveryFailure => e
-    info = discover_google_apps(uri)
-
-    if info.nil?
-      raise e
-    else
-      return info
+      if info.nil?
+        raise e
+      else
+        return info
+      end
     end
   end
 
